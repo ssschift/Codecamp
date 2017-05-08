19285402596a8459b91241728c52cccae4ff2353
Lorena Pati <lorena@pati.ch>
2017-05-08 15:31:31 +0200

    Auto stash before merge of "master" and "origin/master"
    SQL functions

--- system/data.php
+++ system/data.php
@@ -7,4 +7,38 @@
     mysqli_set_charset($db, "utf8");
     return $db;
   }
+
+  function get_result($sql)
+    {
+      $db = get_db_connection();
+      // echo $sql;
+      $result = mysqli_query($db, $sql);
+      mysqli_close($db);
+      return $result;
+    }
+
+    function get_id_result($sql)
+    {
+      $db = get_db_connection();
+      // echo $sql;
+      mysqli_query($db, $sql);
+      $result = mysqli_insert_id($db);
+      mysqli_close($db);
+      return $result;
+    }
+
+
+    function get_modul($freimodul_id)
+    //Abfrage für gewählte Freimodule;
+{
+  $sql = "SELECT *
+          FROM freimodule
+          WHERE (`freimodule`.`freimodul_id` = 2)
+          ORDER BY `freimodule`.`freimodul_id` ASC";
+  return get_result($sql);
+}
+
+
+
+
   ?>
