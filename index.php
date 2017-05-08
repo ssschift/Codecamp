4f1666e1097c54d2f69eada8886fc9ae3cff142a
Laura Glanzmann <laura.glanzmann@mmp.htwchur.ch>
2017-05-08 15:47:28 +0200

    Radio buttons  geändert Script für Info-Flag hinzugefügt

--- index.php
+++ index.php
@@ -39,7 +39,8 @@ require_once('system/security.php');
             </div>
           </div>
           <form method="post" role="form"> <!-- Formularbeignn -->
-            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true"> <!-- Accordion Anfang -->
+              <!--Gruppe 1 -->
               <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="überschriftEins">
                   <h4 class="panel-title">
@@ -50,13 +51,36 @@ require_once('system/security.php');
                 </div>
                 <div id="collapseEins" class="panel-collapse collapse" role="tabpanel" aria-labelledby="überschriftEins">
                   <div class="panel-body">
-                    Radio Button 1 <br>
-                    Radio Button 2 <br>
-                    Radio Button 3 <br>
-                    Radio Button 4 <br>
-                  </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeEins" id="1" value="option1_1">
+                        Filmisches Erzählen
+                      </label> <a class="mod_info"  data-modid="1"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span></a>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeEins" id="2" value="option1_2">
+                        Design Thinking
+                      </label> <a class="mod_info"  data-modid="2"><span class="glyphicon glyphicon-flag" aria-hidden="true"></span></a>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeEins" id="3" value="option1_3">
+                        Live Kommunikation
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeEins" id="10" value="option1_4">
+                        Keine Auswahl
+                      </label>
+                    </div>
+                    <section id="content_gr1">
+                    </section>
+                  </div> <!-- Panelbody fertig -->
                 </div>
               </div>
+              <!--Gruppe 2 -->
               <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="überschriftZwei">
                   <h4 class="panel-title">
@@ -67,13 +91,34 @@ require_once('system/security.php');
                 </div>
                 <div id="collapseZwei" class="panel-collapse collapse" role="tabpanel" aria-labelledby="überschriftZwei">
                   <div class="panel-body">
-                    Radio Button 1 <br>
-                    Radio Button 2 <br>
-                    Radio Button 3 <br>
-                    Radio Button 4 <br>
-                  </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeZwei" id="4" value="option2_1">
+                        Web der Dinge
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeZwei" id="5" value="option2_2">
+                        Postproduction
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeZwei" id="6" value="option2_3">
+                        Markenführung
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeZwei" id="11" value="option2_4">
+                        Keine Auswahl
+                      </label>
+                    </div>
+                  </div> <!-- Panelbody fertig -->
                 </div>
               </div>
+              <!--Gruppe 3 -->
               <div class="panel panel-default">
                 <div class="panel-heading" role="tab" id="überschriftDrei">
                   <h4 class="panel-title">
@@ -84,14 +129,53 @@ require_once('system/security.php');
                 </div>
                 <div id="collapseDrei" class="panel-collapse collapse" role="tabpanel" aria-labelledby="überschriftDrei">
                   <div class="panel-body">
-                    Radio Button 1 <br>
-                    Radio Button 2 <br>
-                    Radio Button 3 <br>
-                    Radio Button 4 <br>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeDrei" id="7" value="option3_1">
+                        Make it Sound
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeDrei" id="8" value="option3_2">
+                        Journalismus
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeDrei" id="9" value="option3_3">
+                        Social Media
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="GruppeDrei" id="12" value="option3_4">
+                        Keine Auswahl
+                      </label>
+                    </div>
+                  </div> <!-- Panelbody fertig -->
+                </div>
+              </div>
+              <!--Gruppe 4 - Abschicken des Formualrs -->
+              <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="überschriftVier">
+                  <h4 class="panel-title">
+                    <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseVier" aria-expanded="false" aria-controls"collapseDrei">
+                      Bestätigen
+                    </a>
+                  </h4>
+                </div>
+                <div id="collapseVier" class="panel-collapse collapse" role="tabpanel" aria-labelledby="überschriftVier">
+                  <div class="panel-body">
+                    <div>
+                        <button type="submit" class="btn btn-default"> Senden </button>
+                    </div>
+                    <br>
+                    Vielen Dank für die Auswahl ihrer Freimodule.
                   </div>
                 </div>
               </div>
-            </div>
+            </div> <!-- Accordion Ende -->
           </form> <!-- Formular Ende -->
         </div>
       </div>
@@ -99,5 +183,11 @@ require_once('system/security.php');
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <!-- JavaScript-Bootstrap -->
     <script src="js/bootstrap.min.js"></script>
+    <script>
+      $(".mod_info").click(function(){
+        console.log($(this).attr("data-modid"));
+        mod_id = $(this).attr("data-modid");
+      })
+    </script>
   </body>
 </html>
