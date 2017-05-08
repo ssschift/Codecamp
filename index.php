306de3f9f17768af7b19f4597dc4448d85ebaeda
Laura Glanzmann <laura.glanzmann@mmp.htwchur.ch>
2017-05-08 10:43:03 +0200

    Erste Anpassung Index, hinzufügen zusätzliches css

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-This is index
+<!DOCTYPE html>
+<html lang="de">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Die drei vorausgehenden meta-Tags *müssen* vor allen anderen Inhalten des head stehen -->
+  <title>Freimodul Wahl</title>
+  <!-- Bootstrap Styles -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <!-- eigene Styles -->
+  <link rel="stylesheet" href="css/freimodulwahl.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <header><h1>Anmeldung Freimodul</h1></header>
+      </div>
+    </div>
+  </div>
+</body>
