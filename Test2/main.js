6833083af5ac3e629548e5b4a010a534982badfe
Lorena Pati <lorena@pati.ch>
2017-05-09 10:30:55 +0200

    Testfiles for AJAX (js and php)

---
+++ Test2/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+  var infoFreimodul = 1;
+
+  $("#infoButton").click(function() {
+
+    $.ajax({
+      // method: "GET",				// type auch ok
+      url: "Test2/infomodul.php",	// Aufruf externer Datei
+      data: {infoFreimodul: imageCounter},	// Übergeben Parameter an PHP
+      dataType: "text",
+      success: function (output) {	// empfangen von PHP
+        imageCounter = imageCounter + 1;
+        $('.images').append(output);
+      }
+    }); // Ajax
+
+  }); // Click Button
+
+}); // Document Ready
